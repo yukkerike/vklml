97399a3eccf2faf5af0c53f6e33b90826fec286c
Иван Козловский <ikozlovskiy24@gmail.com>
2019-11-17 06:02:30 +0300

    Теперь index.html может быть просмотрен и с выключенным js

--- updateIndex.py
+++ updateIndex.py
@@ -3,16 +3,8 @@ import os
 def updateIndex(cwd,prevDate):
         date = time.strftime("%m%y",time.localtime())
         if prevDate == 0:
-                if not os.path.exists(os.path.join(cwd, "mesAct",  "index.html")):
-                        generateIndex(cwd, date)
-                        return
-                else:
-                        f = open(os.path.join(cwd, "mesAct",  'index.html'), 'r')
-                        prevDate = f.read()[4:9]
-                        if prevDate != date:
-                                f.close()
-                                generateIndex(cwd, date)
-                        return
+                generateIndex(cwd, date)
+                return
         elif prevDate == 1:
                 if not os.path.exists(os.path.join(cwd, "mesAct",  "index.html")):
                         generateIndex(cwd, date)
@@ -39,42 +31,42 @@ def generateIndex(cwd, date):
 </head>
 <body>
         <ul>
-                <li hidden><a href="./messages_01{0}.html">messages_01{0}.html</a></li>
-                <li hidden><a href="./messages_02{0}.html">messages_02{0}.html</a></li>
-                <li hidden><a href="./messages_03{0}.html">messages_03{0}.html</a></li>
-                <li hidden><a href="./messages_04{0}.html">messages_04{0}.html</a></li>
-                <li hidden><a href="./messages_05{0}.html">messages_05{0}.html</a></li>
-                <li hidden><a href="./messages_06{0}.html">messages_06{0}.html</a></li>
-                <li hidden><a href="./messages_07{0}.html">messages_07{0}.html</a></li>
-                <li hidden><a href="./messages_08{0}.html">messages_08{0}.html</a></li>
-                <li hidden><a href="./messages_09{0}.html">messages_09{0}.html</a></li>
-                <li hidden><a href="./messages_10{0}.html">messages_10{0}.html</a></li>
-                <li hidden><a href="./messages_11{0}.html">messages_11{0}.html</a></li>
-                <li hidden><a href="./messages_12{0}.html">messages_12{0}.html</a></li>
-                <li hidden><a href="./messages_13{0}.html">messages_13{0}.html</a></li>
-                <li hidden><a href="./messages_14{0}.html">messages_14{0}.html</a></li>
-                <li hidden><a href="./messages_15{0}.html">messages_15{0}.html</a></li>
-                <li hidden><a href="./messages_16{0}.html">messages_16{0}.html</a></li>
-                <li hidden><a href="./messages_17{0}.html">messages_17{0}.html</a></li>
-                <li hidden><a href="./messages_18{0}.html">messages_18{0}.html</a></li>
-                <li hidden><a href="./messages_19{0}.html">messages_19{0}.html</a></li>
-                <li hidden><a href="./messages_20{0}.html">messages_20{0}.html</a></li>
-                <li hidden><a href="./messages_21{0}.html">messages_21{0}.html</a></li>
-                <li hidden><a href="./messages_22{0}.html">messages_22{0}.html</a></li>
-                <li hidden><a href="./messages_23{0}.html">messages_23{0}.html</a></li>
-                <li hidden><a href="./messages_24{0}.html">messages_24{0}.html</a></li>
-                <li hidden><a href="./messages_25{0}.html">messages_25{0}.html</a></li>
-                <li hidden><a href="./messages_26{0}.html">messages_26{0}.html</a></li>
-                <li hidden><a href="./messages_27{0}.html">messages_27{0}.html</a></li>
-                <li hidden><a href="./messages_28{0}.html">messages_28{0}.html</a></li>
-                <li hidden><a href="./messages_29{0}.html">messages_29{0}.html</a></li>
-                <li hidden><a href="./messages_30{0}.html">messages_30{0}.html</a></li>
-                <li hidden><a href="./messages_31{0}.html">messages_31{0}.html</a></li>
+                <li><a href="./messages_01{0}.html">messages_01{0}.html</a></li>
+                <li><a href="./messages_02{0}.html">messages_02{0}.html</a></li>
+                <li><a href="./messages_03{0}.html">messages_03{0}.html</a></li>
+                <li><a href="./messages_04{0}.html">messages_04{0}.html</a></li>
+                <li><a href="./messages_05{0}.html">messages_05{0}.html</a></li>
+                <li><a href="./messages_06{0}.html">messages_06{0}.html</a></li>
+                <li><a href="./messages_07{0}.html">messages_07{0}.html</a></li>
+                <li><a href="./messages_08{0}.html">messages_08{0}.html</a></li>
+                <li><a href="./messages_09{0}.html">messages_09{0}.html</a></li>
+                <li><a href="./messages_10{0}.html">messages_10{0}.html</a></li>
+                <li><a href="./messages_11{0}.html">messages_11{0}.html</a></li>
+                <li><a href="./messages_12{0}.html">messages_12{0}.html</a></li>
+                <li><a href="./messages_13{0}.html">messages_13{0}.html</a></li>
+                <li><a href="./messages_14{0}.html">messages_14{0}.html</a></li>
+                <li><a href="./messages_15{0}.html">messages_15{0}.html</a></li>
+                <li><a href="./messages_16{0}.html">messages_16{0}.html</a></li>
+                <li><a href="./messages_17{0}.html">messages_17{0}.html</a></li>
+                <li><a href="./messages_18{0}.html">messages_18{0}.html</a></li>
+                <li><a href="./messages_19{0}.html">messages_19{0}.html</a></li>
+                <li><a href="./messages_20{0}.html">messages_20{0}.html</a></li>
+                <li><a href="./messages_21{0}.html">messages_21{0}.html</a></li>
+                <li><a href="./messages_22{0}.html">messages_22{0}.html</a></li>
+                <li><a href="./messages_23{0}.html">messages_23{0}.html</a></li>
+                <li><a href="./messages_24{0}.html">messages_24{0}.html</a></li>
+                <li><a href="./messages_25{0}.html">messages_25{0}.html</a></li>
+                <li><a href="./messages_26{0}.html">messages_26{0}.html</a></li>
+                <li><a href="./messages_27{0}.html">messages_27{0}.html</a></li>
+                <li><a href="./messages_28{0}.html">messages_28{0}.html</a></li>
+                <li><a href="./messages_29{0}.html">messages_29{0}.html</a></li>
+                <li><a href="./messages_30{0}.html">messages_30{0}.html</a></li>
+                <li><a href="./messages_31{0}.html">messages_31{0}.html</a></li>
         </ul>
         <script>
                 list = document.getElementsByTagName("li")
-                var now = new Date()
-                for (i = 0; i < now.getDate(); i++) list[i].removeAttribute("hidden");
+                var now = new Date().getDate()
+                for (i = 30; i >= now; i--) list[i].setAttribute("hidden","");
         </script>
 </body>
 </html>""".format(date))
