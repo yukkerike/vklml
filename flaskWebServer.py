92ae6c012dee6ce1d823162ab75df6ed8a5be333
Иван Козловский <ikozlovskiy24@gmail.com>
2020-03-07 18:07:18 +0300

    Изменение При использовании встроенного веб-сервера, если включена авторизация, токен в файле vkGetVideoLink.html, будет отсутствовать, если выполнена авторизация не от имени первого в списке пользователя.

--- flaskWebServer.py
+++ flaskWebServer.py
@@ -62,5 +62,52 @@ def index():
 def send(path):
     return send_from_directory('mesAct', path)
 
+@app.route("/vkGetVideoLink.html")
+def video():
+    if config['useAuth']:
+        auth = request.authorization
+        if not auth or not check_auth(auth.username, auth.password):
+            return Response(
+            'Could not verify your access level for that URL.\n'
+            'You have to login with proper config', 401,
+            {'WWW-Authenticate': 'Basic realm="Login Required"'})
+        else:
+            return """<!DOCTYPE html>
+    <html>
+        <head>
+            <meta charset="utf-8">
+        </head>
+        <body>
+            <input id="videos"></input>
+            <input type="submit" id="submit" value="Отправить">
+            <div><p>Если видео не проигрывается, прямую ссылку можно получить через api:</p></div>
+            <div style="position:relative;padding-top:56.25%;"></div>
+            <script>
+                let ACCESS_TOKEN = '{}';
+                document.getElementById('submit').onclick = function() {{
+                    var link = document.createElement('a');
+                    link.href = "https://vk.com/dev/video.get?params[videos]=0_0," + videos.value + "&params[count]=1&params[offset]=1";
+                    link.innerText = videos.value;
+                    document.getElementById('submit').disabled = true;
+                    document.getElementsByTagName("div")[0].appendChild(link);
+                    var script = document.createElement('SCRIPT');
+                    script.src = "https://api.vk.com/method/video.get?v=5.101&access_token=" + ACCESS_TOKEN + "&videos=" + videos.value + "&callback=callbackFunc";
+                    document.getElementsByTagName("head")[0].appendChild(script);
+                }}
+                function callbackFunc(result) {{
+                    var frame = document.createElement('iframe');
+                    frame.src = result.response.items[0]["player"];
+                    frame.style = "position:absolute;top:0;left:0;width:100%;height:100%;";
+                    document.getElementsByTagName("div")[1].appendChild(frame);
+                }}
+                let videos = document.getElementById('videos');
+                videos.value = document.location.search.slice(1);
+                if (videos.value != "") document.getElementById('submit').click()
+            </script>
+        </body>
+    </html>""".format(config['ACCESS_TOKEN'] if list(config['users']).index(auth.username) == 0 else "")
+    else:
+        return send_from_directory('mesAct', 'vkGetVideoLink.html')
+
 if __name__ == "__main__":
     app.run()
